816373025a55dbf1e4c92758a76b926e2e8a04d7
yuting1214 <l501l501l@gmail.com>
2024-06-18 23:29:54 -0400

    Final Version

--- backend/app/api/v1/endpoints/base.py
+++ backend/app/api/v1/endpoints/base.py
@@ -7,15 +7,3 @@ router = APIRouter()
 @router.get("/")
 def onboard_message():
     return {"message": "You've been onboarded!"}
-
-@router.get("/env")
-def print_env():
-    return {"username": os.getenv("USER_NAME"),
-            "password": os.getenv("PASSWORD"),
-            "DB_ENGINE": os.getenv("DB_ENGINE"),
-            "DB_USERNAME": os.getenv("DB_USERNAME"),
-            "DB_PASS": os.getenv("DB_PASS"),
-            "DB_HOST": os.getenv("DB_HOST"),
-            "DB_PORT": os.getenv("DB_PORT"),
-            "DB_NAME": os.getenv("DB_NAME")
-            }
