d0abc2cb0b90b1f9baa2def3bd5b601aaf0e668c
yuting1214 <l501l501l@gmail.com>
2024-06-18 23:18:46 -0400

    chore: Add endpoint to print environment variables

--- backend/app/api/v1/endpoints/base.py
+++ backend/app/api/v1/endpoints/base.py
@@ -8,7 +8,14 @@ router = APIRouter()
 def onboard_message():
     return {"message": "You've been onboarded!"}
 
-@router.get("/test")
-def print_username_password():
-    return {"username": os.getenv("USER_NAME"), "password": os.getenv("PASSWORD")}
-
+@router.get("/env")
+def print_env():
+    return {"username": os.getenv("USER_NAME"),
+            "password": os.getenv("PASSWORD"),
+            "DB_ENGINE": os.getenv("DB_ENGINE"),
+            "DB_USERNAME": os.getenv("DB_USERNAME"),
+            "DB_PASS": os.getenv("DB_PASS"),
+            "DB_HOST": os.getenv("DB_HOST"),
+            "DB_PORT": os.getenv("DB_PORT"),
+            "DB_NAME": os.getenv("DB_NAME")
+            }
