23a8d132eb084e01aca38c57f000d34131190675
yuting1214 <l501l501l@gmail.com>
2024-06-18 23:13:20 -0400

    Add testing function

--- backend/app/api/v1/endpoints/base.py
+++ backend/app/api/v1/endpoints/base.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 
@@ -6,3 +7,8 @@ router = APIRouter()
 @router.get("/")
 def onboard_message():
     return {"message": "You've been onboarded!"}
+
+@router.get("/test")
+def print_username_password():
+    return {"username": os.getenv("USER_NAME"), "password": os.getenv("PASSWORD")}
+
